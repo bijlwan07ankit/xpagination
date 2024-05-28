f1da93924c75a3e1c0ab9194fe592325ba620281
bijlwan07ankit <bijlwan07ankit@gmail.com>
2024-05-28 20:16:28 +0530

    final code

--- xpagination/src/App.jsx
+++ xpagination/src/App.jsx
@@ -1,15 +1,59 @@
-import { useState } from 'react'
-import Pagination from "./Components/Pagination"
-
-
-function App() {
-  
-
+import { useEffect, useState } from "react";
+import "./Styles.css";
+import axios from "axios";
+import Pagination from "../src/Components/Pagination";
+export default function App() {
+  let api =
+    "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+  let [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recordsPerPage] = useState(10);
+  const indexOfLastRecord = currentPage * recordsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+  useEffect(() => {
+    (async function () {
+      try {
+        let res = await axios.get(api);
+        setData(res.data);
+        console.log(res.data);
+      } catch (error) {
+        console.log(alert("failed to fetch data"));
+      }
+    })();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const currentRecords = data.slice(indexOfFirstRecord, indexOfLastRecord);
+  const nPages = Math.ceil(data.length / recordsPerPage);
   return (
-    <>
-      <Pagination /> 
-    </>
-  )
+    <div className="App">
+      <h1>Employee Data Table</h1>
+      <table>
+        <thead>
+          <tr className="main-col">
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody id="tableBody">
+        {currentRecords.map((person) => {
+            return (
+              <tr>
+                <th>{person.id}</th>
+                <th>{person.name}</th>
+                <th>{person.email}</th>
+                <th>{person.role}</th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <Pagination
+        nPages={nPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+    </div>
+  );
 }
-
-export default App
--- xpagination/src/Components/Pagination.jsx
+++ xpagination/src/Components/Pagination.jsx
@@ -1,71 +1,19 @@
-import React from 'react'
-import {useState, useEffect} from "react";
-import styles from "./Pagination.module.css"
-import axios from "axios" ;
-import Footer from './footer';
-function Pagination() {
-const [data, setData] = useState([]);
-const [currentPage,setCurrentPage]=useState(1);
-const [recordPerPage , setRecordPerPage]=useState(10);
-const [indexOfLastrecord, setIndexOfLastrecord]=currentPage*recordPerPage();
-const [indexOfFirstrecord, setIndexOfFirstRecord]=indexOfLastrecord-recordPerPage   
+import React from "react";
 
+const Pagination = ({ nPages, currentPage, setCurrentPage }) => {
+  const goToNextPage = () => {
+    if (currentPage !== nPages) setCurrentPage(currentPage + 1);
+  };
+  const goToPrevPage = () => {
+    if (currentPage !== 1) setCurrentPage(currentPage - 1);
+  };
+  return (
+    <div className="BtnDiv">
+      <button onClick={goToPrevPage}>Previous</button>
+      <h5 className="page">{currentPage}</h5>
+      <button onClick={goToNextPage}>Next</button>
+    </div>
+  );
+};
 
-useEffect(()=>{
-    
-    fetchData();
-},[])
-
-const fetchData=( async ()=>{
-    try{
-        const res = await axios.get(" https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json") ;
-        setData(res.data);
-console.log(data);
-    }
-    catch(error){
-        console.log("can not fetch Api ", error)
-    }
-})
-const currentRecords=data.slice(indexOfFirstrecord,indexOfLastrecord);
-const npages=Math.ceil(data.length/recordPerPage);
-
-    return (
-<>
-<h1 >Employee Data Table</h1> 
-<table className={styles.table}>
-    
-    <thead className={styles.head}>
-<tr>
-    <td>ID</td>
-    <td>Name</td>
-    <td>Email</td>
-    <td>role</td>
-</tr>
-    </thead>
-    
-    <tbody id={styles.tbody}>
-       {currentRecords.map((person)=>{
-            return (
-                <tr>
-        <th>{person.id}</th>
-        <th>{person.name}</th>
-        <th>{person.email}</th>
-        <th>{person.role}</th>
- </tr>
-        )
-        })}
-    </tbody>    
-</table>
-<Footer
-        nPages={npages}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
-
-
-</>
-   
-  )
-}
-
-export default Pagination
+export default Pagination;
